7664b427907c10fa2aaf750a8e75abdbfde6f948
pushline <kauebbortolatto@gmail.com>
2021-01-04 19:49:15 -0300

    Login com auth (breeze)

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,12 @@ Route::get('posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+require __DIR__.'/auth.php';
